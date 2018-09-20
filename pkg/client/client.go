dba2d754cbef7be65753568b70a08fe008f25af8
volodimyr <l.volodimyr@gmail.com>
2018-09-20 15:42:17 +0300

    Publisher set timeout for client and modify return statement

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -4,16 +4,26 @@ import (
 	"bytes"
 	"log"
 	"net/http"
+	"time"
+)
+
+var (
+	timeout = time.Second * 3
+
+	client = &http.Client{Timeout: timeout}
 )
 
 //DoPOST uses for making http POST request to a specific URL
-func DoPOST(URL string, body []byte, logger *log.Logger) {
-	resp, err := http.Post(URL, "application/json", bytes.NewBuffer(body))
+//client has set timeout for 3 seconds
+func DoPOST(URL string, body []byte, logger *log.Logger) (*http.Response, error) {
+	resp, err := client.Post(URL, "application/json", bytes.NewBuffer(body))
 	if err != nil {
 		logger.Println("Couldn't send a request to server.", err)
-		return
+		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
 		logger.Printf("Sent message, but listener gave respond with status code: [%d]", resp.StatusCode)
 	}
+
+	return resp, err
 }
